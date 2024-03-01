4a38b0bd943b0093026b4cb7e9ab959d533eb7e4
SatishSingh0313 <satishsangam.205@gmail.com>
2024-03-02 01:11:39 +0530

    Swap Two Number

--- swaps/Swap1.java
+++ swaps/Swap1.java
@@ -1,10 +1,11 @@
-package com.swaps.number;
+package swaps.number;
 
 public class Swap1 {
 	public static void main(String[] args) {
 		int a=10 ,b=20, temp=0;
 		
 		System.out.println("Before Swap a="+a+" b="+b);
+		
 		temp=a;
 		a=b;
 		b=temp;
