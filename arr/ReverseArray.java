0e5c744ee48bc20e0b6412986a59489f8d132509
SatishSingh0313 <satishsangam.205@gmail.com>
2024-03-02 00:18:32 +0530

    ReverseArray

--- arr/ReverseArray.java
+++ arr/ReverseArray.java
@@ -2,9 +2,11 @@ package com.arr;
 
 public class ReverseArray {
 	public static void main(String[] args) {
+
 		int arr[] = new int[] {10,20,30,40,50};
 		for(int i=arr.length-1;i>=0;i--) 
-			System.out.println(arr[i]+" ");
+
+			System.out.print(arr[i]+" ");
 		}
 	
 }
